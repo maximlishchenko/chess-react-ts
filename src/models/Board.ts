07a189c238b6e5487a5a07d9bc6d239db355d766
maximlishchenko <makslis2000@gmail.com>
2023-08-01 18:00:17 +0300

    add king movement logic

--- src/models/Board.ts
+++ src/models/Board.ts
@@ -15,7 +15,7 @@ export class Board {
   lostWhiteFigures: Figure[] = [];
 
   public initCells() {
-    for (let i = 0; i < 8; i ++) {
+    for (let i = 0; i < 8; i++) {
       const row: Cell[] = [];
       for (let j = 0; j < 8; j++) {
         if ((i + j) % 2 !== 0) {
